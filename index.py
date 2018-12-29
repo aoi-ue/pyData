b0f00b3a18c50109ce936f3601c02f6f3a9bcad5
silvsice <sans-23@hotmail.com>
2018-12-29 17:10:11 +0800

    added in the main excel dataframe and library to run the dataframe

--- index.py
+++ index.py
@@ -1,1 +1,7 @@
-print ('hello sandeep')
+import xlrd
+import pandas as pd
+
+xlsfile = pd.ExcelFile('E:\\Python Projects\\pyData\\DataDump\\SINGTEL_INGESTION_EXTRACT_2018-12-26.xlsx')
+dframe = xlsfile.parse('Sheet1')
+
+print(dframe['lic_start_date_india'])
