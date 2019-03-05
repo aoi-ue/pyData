d1196626ada79d02d79e182426c506d5846080bb
yuhanyuhan <yuhanlian@gmail.com>
2019-03-05 21:16:29 +0800

    Initialized Web Server to run Pandas and Seaborn to coupled with SQLite and S3

---
+++ webserver.py
@@ -0,0 +1,16 @@
+import requests 
+
+# pyServer 
+# Create a web server using BASEHttpServer, and convert to Requests lib
+# Running All Locally -- 
+# Create S3 Bucket with mock excel file 
+# Replace with Excel Data, Call S3 Data with using AWS CLI 
+
+# Pandas
+# Create Objects for excel data to run Panda 
+# Store output data in SQLite/MySQL 
+
+# Seaborn 
+# Create Class for Panda data to run Seaborn 
+# Send Response Request to Web Front End 
+# Send response to S3, and creating new folder to store images based on the date created 
