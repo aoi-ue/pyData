612ec8b46912933f75476d6e03ebb182cfc66621
yuhanyuhan <yuhanlian@gmail.com>
2019-03-17 19:39:00 +0800

    inplemented Requests for get and post request

--- webserver.py
+++ webserver.py
@@ -1,9 +1,13 @@
+# pyServer 
 import requests 
 
-# pyServer 
+#get S3 bucket's excel sheet 
+r = requests.get('https://api.github.com/user', auth=('user', 'pass'))
+
+
 # Create a web server using BASEHttpServer, and convert to Requests lib
-# Create S3 Bucket with mock excel file 
-# Replace with Excel Data, Call S3 Data with using AWS CLI 
+# Create S3 Bucket for excel file 
+# Create a listening port for S3 to extract data, using AWS CLI 
 
 # Pandas
 # Create Objects for excel data to run Panda 
@@ -13,3 +17,5 @@ import requests
 # Create Class for Panda data to run Seaborn 
 # Send Response Request to Web Front End 
 # Send response to S3, and creating new folder to store images based on the date created 
+
+# pyTest 
