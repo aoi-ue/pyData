b41b50d9f764b0f3b37a8655d885fbc4862464e9
yuhanyuhan <yuhanlian@gmail.com>
2019-04-30 12:57:06 +0800

    Finalize Web Server component

--- webserver.py
+++ webserver.py
@@ -1,21 +1,20 @@
 # pyServer 
 import requests 
+import json
 
-#get S3 bucket's excel sheet 
+# Create/intialize a web server using BASEHttpServer, and convert to Requests/Bottle/Flask lib
+# Create a on-listen HTTP port for S3 to extract data 
+# Get S3 bucket's excel sheet, using AWS CLI 
 r = requests.get('https://api.github.com/user', auth=('user', 'pass'))
+data = json.loads(r.content)
+print (data)
 
+# Parse Raw Data to Pandas and put in async while running the following.. 
+# Local machine to create a new SQL table to set up sql schema 
+# Call for instances from EC2 Store output data in SQLite/MySQL 
 
-# Create a web server using BASEHttpServer, and convert to Requests lib
-# Create S3 Bucket for excel file 
-# Create a listening port for S3 to extract data, using AWS CLI 
-
-# Pandas
-# Create Objects for excel data to run Panda 
-# Store output data in SQLite/MySQL 
-
-# Seaborn 
-# Create Class for Panda data to run Seaborn 
-# Send Response Request to Web Front End 
-# Send response to S3, and creating new folder to store images based on the date created 
-
-# pyTest 
+# Parse SQL data to Seaborn 
+# Send HTTP Request to Web Front End of Seaborn with Flask -> Django for Full Stack Application to manage data set creation
+# Set up Nginx Server to serve forever
+# Create S3 Bucket to store and manage excel file, using python scripting 
+# Write pyTest for every component 
