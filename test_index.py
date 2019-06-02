58e26b033a88605bc971e5b5d85826499c267707
silvsice <sans-23@hotmail.com>
2019-06-02 17:49:08 +0800

    added test cases

--- test_index.py
+++ test_index.py
@@ -1,5 +1,5 @@
 import pytest
-from index import excelname
+from index import df, excelname
 from refactor_test import subtitle_length, subtitle_unique_length, subtitle_list
 
 def test1():
@@ -11,3 +11,7 @@ def test1():
 def test2():
     text = "csv"
     assert text in excelname
+
+#Checking that the dataframe is not empty
+def test3():
+    assert df is not df.empty
