dff32cf42a877bd3611fd709807de9e4e4babef8
silvsice <sans-23@hotmail.com>
2019-07-22 18:32:28 +0800

    deleted

--- s3.py
+++
@@ -1,8 +0,0 @@
-import boto3
-import subprocess
-import pandas as pd
-import io
-
-s3 = boto3.client('s3')
-obj = s3.get_object(Bucket='excelbkt', Key='MOCK_SINGTEL_INGESTION_EXTRACT_2018-12-26.csv')
-df = pd.read_csv(io.BytesIO(obj['Body'].read()))
